3271a244a5563785beadafc2fe42a72d09c09c0a
unknown <jamessamantha099@gmail.com>
2020-11-29 21:28:39 -0600

    made changes to php file to connect to db

--- dbConnection.php
+++ dbConnection.php
@@ -2,8 +2,8 @@
 
 require_once 'config.php';
 
-
+try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=$charset", $username, $password);
-
+} catch (PDOException $pe) {
     die("Could not connect to the database $dbname :" . $pe->getMessage());
 }
