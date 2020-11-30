6d134ac8846a18c0d78cfce4af730dbfb5d28d42
unknown <jamessamantha099@gmail.com>
2020-11-30 02:30:58 -0600

    finished creating issue and now started creating submission php

--- createIssue.php
+++ createIssue.php
@@ -11,11 +11,48 @@ if(isset($_GET)){
         <h1>Create Issue<h1>
         <div id = \"issue\">
             <label for = \"issue\">Title</label><br>
-            <input type = \"text\" id = \"newissue\" name = \"title\"/><br>
+            <input type = \"text\" id = \"issueTitle\" name = \"title\"/><br>
         
         </div>
         <div id =\"des\">
             <label for = \"des\">Description</label><br>
-        
-</div>
+            <textarea maxlength = \"300\" rows = \"5\" cols = \"50\" id = \"newissueDes\" name = \"des\"></textarea><br>
+
+        </div>
+
+        <div id = \"assign\">
+            <label for = \"assign\"> Assigned To</label><br>
+            select id = \"issueAssign\" name = \"assign\" placeholder = \"Marcia Brady\">";
+            foreach($result as $row) {
+                $name = $row['firstname'] . " " . $row['lastname'];
+                $output .= "<option>" . $name . "</option>";
+            }
+
+$output .= "</select>
+
+        </div>
+
+        <div id = \"priority\">
+            <label for = \"priority\">Priority</label><br>
+
+            <select id = \"newissue-priority\" name = \"priority\" placeholder = \"Major\">
+                <option value = \"Minor\">Minor</option>
+                <option value = \"Major\">Major</option>
+                <option value = \"Critical\">Critical</option>
+            </select>
+        </div>
+        <div id = \"submission\">
+            <p>
+                <input type = \"button\" id = \"newissue-submit\" value=\"Submit\"/>
+            </p>
+    
+         </div>
+
+        </form>
+    </div>";
+   
+    echo $output;
+
+
 }
+?>
